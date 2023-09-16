90b7669817fbaad522122d868194a6e3b75db3b7
MarcoVitoC <marcovito.chr@gmail.com>
2023-09-16 22:40:20 +0700

    feat: add solution for `Odd Set`

---
+++ Codeforces/2023/September/OddSet.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+   int n;
+   cin >> n;
+
+   n *= 2;
+   int a[n], odd = 0, even = 0;
+   for (int i=0; i<n; i++) {
+      cin >> a[i];
+      if (a[i] % 2 == 0) {
+         even++;
+      } else {
+         odd++;
+      }
+   }
+
+   cout << (odd == even ? "Yes" : "No") << endl;
+}
+
+int main() {
+   int t;
+   cin >> t;
+
+   while (t--) {
+      solve();
+   }
+}
