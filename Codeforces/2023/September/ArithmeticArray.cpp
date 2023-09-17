b6e4dda727b8b313ee874a490bb458aac06f51b0
MarcoVitoC <marcovito.chr@gmail.com>
2023-09-17 13:06:57 +0700

    feat: add solution for `Arithmetic Array`

---
+++ Codeforces/2023/September/ArithmeticArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+   int n;
+   cin >> n;
+
+   int a[n], sum = 0;
+   for (int i=0; i<n; i++) {
+      cin >> a[i];
+      sum += a[i];
+   }
+
+   int minNumToAppend = (sum < n) ? 1 : sum - n;
+   cout << minNumToAppend << endl;
+}
+
+int main() {
+   int t;
+   cin >> t;
+
+   while (t--) {
+      solve();
+   }
+}
