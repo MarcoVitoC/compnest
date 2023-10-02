9a9f76806abb8192e57c58f64cdd41fad21649ba
MarcoVitoC <marcovito.chr@gmail.com>
2023-10-02 22:10:04 +0700

    feat: add solution for `The Last Non-zero Digit`

---
+++ College/Math/TheLastNon-ZeroDigit.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main() {
+   ll N, M;
+   
+   while (cin >> N >> M) {
+      ll res = 1;
+      
+      if (N == 0) {
+         cout << 1 << endl;
+      } else {
+         for (int i=N; i>N-M; i--) {
+            res *= i;
+            while (res % 10 == 0) {
+               res /= 10;
+            }
+            res %= 10000000000; // prevent res value from becoming too large
+         }
+         cout << res % 10 << endl;
+      }
+   }
+
+   return 0;
+}
